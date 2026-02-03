758c957e7715bad899b030e707f87bd6d046106a
carlos-cas3 <ccastillob44@gmail.com>
2026-02-03 03:33:06 -0500

    safd

--- provision_client.sh
+++ provision_client.sh
@@ -3,5 +3,8 @@ set -e
 
 sudo mount master:/home/vagrant/PCyP /home/vagrant/PCyP || true
 
+if ! grep -q PCyP /etc/fstab; then
+  echo "master:/home/vagrant/PCyP /home/vagrant/PCyP nfs defaults 0 0" | sudo tee -a /etc/fstab
+fi
 
 echo "Cliente listo."
